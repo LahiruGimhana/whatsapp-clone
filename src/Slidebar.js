02077b35ce7f09eed32a0cc0420a4f96310868b0
lahirugimhana95@gmail.com <lahirugimhana95@gmail.com>
2021-08-08 19:17:17 +0530

    start whatsappp clone

---
+++ src/Slidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Slidebar.css';
+
+function Slidebar() {
+    return (
+        <div className="slidebar">
+            hello
+        </div>
+    )
+}
+
+export default Slidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB517cgMTByVYJ4p-5WlW7N7k3yj_pOjsg",
+  authDomain: "whatsapp-mern-9e18c.firebaseapp.com",
+  projectId: "whatsapp-mern-9e18c",
+  storageBucket: "whatsapp-mern-9e18c.appspot.com",
+  messagingSenderId: "347159776576",
+  appId: "1:347159776576:web:5b8ea705839f1e2c94bea5",
+  measurementId: "G-3JQKETKLNP"
+};
